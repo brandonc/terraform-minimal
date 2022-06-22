35507528063ce5d95383e4107dbc9bf12df99f00
Brandon Croft <brandon.croft@gmail.com>
2022-06-22 10:49:31 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-variable "foo" {
-  type = string
-  default = "bar"
-}
-
-output "output_of_run_variable" {
-  value = {
-    my_foo_value = var.foo
-  }
-}
-
 resource "null_resource" "null" {
   triggers = {
     hello: "world"
